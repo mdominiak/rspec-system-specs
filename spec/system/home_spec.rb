98595d6a67877598eca5425796db2bf433eae6fe
Mateusz Dominiak <mateusz.dominiak@gmail.com>
2017-12-29 10:14:43 +0100

    Exemplify database rollback in system specs

--- spec/system/home_spec.rb
+++ spec/system/home_spec.rb
@@ -1,12 +1,19 @@
 require 'rails_helper'
 
-describe 'Home page' do
+describe 'Homepage' do
   before do
     driven_by :selenium_chrome_headless
+    puts 'Planet.create!'
+    Planet.create!(name: 'Mars')
   end
 
   it 'shows greeting' do
     visit root_url
     expect(page).to have_content 'Hello Universe'
+  end
+
+  it 'shows planet count' do
+    visit root_url
+    expect(page).to have_content 'Planet count: 1'
   end
 end
